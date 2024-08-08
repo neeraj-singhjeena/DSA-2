f1bafadc4b38cc40cba8dab5d90dcc30f1cdff46
neeraj-singhjeena <151987844+neeraj-singhjeena@users.noreply.github.com>
2024-08-08 18:33:10 +0530

    "Removed .gitignore file and Traversing-array.exe binary file"

---
+++ ARRAY/Insertion.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, n, num, pos;
+    int arr[20];
+    cout << "enter the size of an array:";
+    cin >> n;
+
+    cout << "enter the elements of an array:" << endl;
+    for (int i = 0; i <= n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "enter the num or value to be insert:";
+    cin >> num;
+
+    cout << "enter the position of an array:";
+    cin >> pos;
+
+    // for (int i = 0; i > pos; i++)
+    // {
+    //     arr[i] = arr[i + 1];
+    // }
+    arr[pos] = num;
+
+    for (int i = 0; i <= n; i++)
+    {
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
