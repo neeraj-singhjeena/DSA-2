707f5beb7ace98a491251e6a833149ed4438b521
neeraj-singhjeena <151987844+neeraj-singhjeena@users.noreply.github.com>
2024-08-11 17:37:23 +0530

    practice

---
+++ ARRAY/traversion.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n, arr[20];
+
+    cout << "enter the size of array:";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    // for (int i = 0; i < n - 1; i++)
+    // {
+    //     for (int j = i + 1; j < n; j++)
+    //     {
+    //         if (arr[j] < arr[i])
+    //         {
+    //             int temp = arr[j];
+    //             arr[j] = arr[i];
+    //             arr[i] = temp;
+    //         }
+    //     }
+    // }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
